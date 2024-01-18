e9c3ee7aaea30c740b89064c88d146fed6c58bc3
murakami97 <murakami.yuya@moneyforward.co.jp>
2024-01-18 15:16:26 +0900

    get all channels if over 100 by repeating request

--- update.go
+++ update.go
@@ -8,14 +8,37 @@ import (
 func updateChannels() {
 	wf.NewItem("Update Channels").Valid(true)
 
+	println("START update channels")
 	c := aw.NewCache(cache_dir)
 	cfg := aw.NewConfig()
 	token := cfg.Get("SLACK_TOKEN")
 	api := slack.New(token)
 	params := slack.GetConversationsForUserParameters{
-    UserID: cfg.Get("USER_ID"),
-}
-	channels, _, err_channels := api.GetConversationsForUser(&params)
+		UserID: cfg.Get("USER_ID"),
+	}
+	var channels []slack.Channel
+	var next_cursor string
+	var cnt int = 0
+	var err_channels error
+
+	for {
+		var newChannels []slack.Channel
+
+		if next_cursor != "" {
+			params.Cursor = next_cursor
+		}
+
+		newChannels, next_cursor, err_channels = api.GetConversationsForUser(&params)
+		println(next_cursor)
+
+		channels = append(channels, newChannels...)
+
+		cnt++
+		println(cnt)
+		if next_cursor == "" || cnt > 10 {
+			break
+		}
+	}
 	team, err_team := api.GetTeamInfo()
 
 	if err_channels != nil || err_team != nil {
